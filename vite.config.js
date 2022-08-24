33e74d7bd473d3698f7be0ef3eae504a08ecf065
cykreet <cykreet@gmail.com>
2022-08-24 13:39:43 +0200

    move deploy script to github directory

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import { Mode, plugin as markdownPlugin } from 'vite-plugin-markdown';
 
 /** @type {import('vite').UserConfig} */
 const config = {
+  base: '/cykreet.com/',
   plugins: [sveltekit(), markdownPlugin({ mode: [Mode.HTML] })],
 };
 
