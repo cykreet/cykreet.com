5bdb9a5be88f9e02a9960b1c6fa5c1030a79b20c
cykreet <cykreet@gmail.com>
2022-08-26 14:25:13 +0200

    fix: svelte static adapter

--- svelte.config.js
+++ svelte.config.js
@@ -1,11 +1,20 @@
-import adapter from "svelte-adapter-bun";
-import preprocess from "svelte-preprocess";
+import adapter from 'svelte-adapter-bun';
+import preprocess from 'svelte-preprocess';
 
+/** @type {import("@sveltejs/kit").Config} */
 export default {
-	preprocess: preprocess({
-		postcss: true,
-	}),
-	kit: {
-		adapter: adapter(),
-	},
+  preprocess: preprocess({
+    postcss: true,
+  }),
+  kit: {
+    adapter: adapter({
+      pages: 'build',
+      assets: 'build',
+      fallback: null,
+      precompress: false,
+    }),
+    prerender: {
+      default: true,
+    },
+  },
 };
