5c8cb63cd67b4efcbd91436e64fd440eeb4d4eb1
cykreet <cykreet@gmail.com>
2022-08-29 18:49:50 +0200

    fix: image paths

--- svelte.config.js
+++ svelte.config.js
@@ -1,6 +1,8 @@
 import staticAdapter from 'svelte-adapter-bun';
 import sveltePreprocess from 'svelte-preprocess';
 
+const production = process.env.BUN_ENV === 'production';
+
 /** @type {import("@sveltejs/kit").Config} */
 export default {
   preprocess: sveltePreprocess({
@@ -14,7 +16,7 @@ export default {
       precompress: false,
     }),
     paths: {
-      base: '/cykreet.com',
+      base: production ? '/cykreet.com' : undefined,
     },
     prerender: {
       default: true,
