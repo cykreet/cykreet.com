b086ffdc2cb22d0273c8c715b15cb91c69db21f4
cykreet <cykreet@gmail.com>
2025-01-03 19:08:10 +0200

    feat: preload site links

--- src/lib/link.svelte
+++ src/lib/link.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+	import { goto, preloadData } from "$app/navigation";
 	import clsx from "clsx";
 
 	export let href: string;
@@ -10,6 +11,23 @@
 	const classNames = clsx("cursor-pointer", className);
 </script>
 
-<a class={classNames} {target} {href} {title}>
-	<slot />
-</a>
+{#if external === false}
+	<!-- svelte-ignore a11y-no-static-element-interactions -->
+	<!-- svelte-ignore a11y-click-events-have-key-events -->
+	<!-- svelte-ignore a11y-mouse-events-have-key-events -->
+	<a
+		{href}
+		class={classNames}
+		on:mouseover={async () => await preloadData(href)}
+		on:click={(event) => {
+			goto(href);
+			event.preventDefault();
+		}}
+	>
+		<slot />
+	</a>
+{:else}
+	<a class={classNames} {target} {href} {title}>
+		<slot />
+	</a>
+{/if}
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -5,13 +5,15 @@
 	import Pill from "$lib/pill.svelte";
 	import clsx from "clsx";
 	import { onMount } from "svelte";
-	import "../app.css";
-	import { goto, preloadData } from "$app/navigation";
 	import { ArrowLeftIcon } from "svelte-feather-icons";
+	import "../app.css";
+	import ArtistsList from "../lib/artists-list/artists-list.svelte";
 	import EventList from "../lib/event-list/event-list.svelte";
+	import Link from "../lib/link.svelte";
 	import TimeIcon from "../lib/time-icon.svelte";
 	import { pageContext } from "../store";
 	import type { LayoutData } from "./$types";
+	import { onNavigate } from "$app/navigation";
 
 	export let data: LayoutData;
 
@@ -36,14 +38,24 @@
 		backPath = document.referrer.split(location.origin)[1]! ?? pageLocation.split("/").slice(0, -1).join("/");
 		return () => clearInterval(timeInterval);
 	});
+
+	onNavigate((navigation) => {
+		if (!document.startViewTransition) return;
+		return new Promise((resolve) => {
+			document.startViewTransition(async () => {
+				resolve();
+				await navigation.complete;
+			});
+		});
+	});
 </script>
 
 <main>
 	<div class="mx-10">
 		<Header />
-		<div class="mt-4 flex flex-col md:flex-row w-full space-x-5 h-[90vh]">
+		<div class="mt-4 flex flex-col-reverse justify-end md:flex-row w-full md:space-x-5 md:space-y-0 space-y-5 h-[90vh]">
 			<div class="md:w-8/12 space-y-5 flex flex-col">
-				<Card cardClassName={homeCardClasses}>
+				<Card cardClassName={homeCardClasses} hoverEffects>
 					<div class="max-w-lg space-y-4">
 						<h1 class="text-inherit text-salmon">
 							<span>welome to my</span>
@@ -55,22 +67,21 @@
 						<h3>hi, i'm jaden. i go by cykreet in most online spaces and i'm currently a student in south africa.</h3>
 					</div>
 				</Card>
-				<div class="flex flex-row space-x-5 h-1/2">
-					<Card cardClassName="w-full" cardTitle="Recent Activity" hoverEffects>
+				<div class="flex flex-col md:flex-row md:space-x-5 md:space-y-0 space-y-5 h-1/2">
+					<Card cardClassName="w-full" cardTitle="Recent Commits" hoverEffects>
 						<EventList events={data.events} />
 					</Card>
-					<Card cardClassName="w-full" cardTitle="Card Dos" hoverEffects>box two</Card>
+					<Card cardClassName="w-full" cardTitle="Listening Activity" hoverEffects>
+						<ArtistsList artists={data.artists} />
+					</Card>
 				</div>
 			</div>
-			<Card cardClassName="md:w-4/12 card-gradient" className="flex flex-col space-y-6" hoverEffects>
+			<Card cardClassName="md:w-4/12 card-gradient" className="flex flex-col space-y-6">
 				<span class="animate-top inline-flex items-center space-x-4 font-medium">
 					{#if (pageLocation.match(/\//gi) || []).length > 1 && backPath}
-						<!-- svelte-ignore a11y-no-static-element-interactions -->
-						<!-- svelte-ignore a11y-click-events-have-key-events -->
-						<!-- svelte-ignore a11y-mouse-events-have-key-events -->
-						<div class="hover:text-white cursor-pointer" on:mouseover={async () => await preloadData(backPath)} on:click={() => goto(backPath)}>
+						<Link href={backPath} className="hover:text-white">
 							<ArrowLeftIcon class="w-5 h-5" />
-						</div>
+						</Link>
 					{/if}
 					<Pill className="w-full min-w-20 overflow-ellipsis overflow-x-hidden">
 						<span class="relative inline-flex h-2 w-2 m-0 mr-2">
@@ -80,7 +91,7 @@
 						<span>{pageLocation}</span>
 					</Pill>
 					<span class="text-sm text-grey-200 inline-flex w-full justify-between">
-						<span class="text-ellipsis">$ {pageDescription ?? "..."}</span>
+						<span class="text-ellipsis">{pageDescription ? `$ ${pageDescription}` : ""}</span>
 						<span>[{hours}:{minutes}:{seconds}]</span>
 					</span>
 				</span>
