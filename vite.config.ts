f14a6f65c3b63bdd561c41e17ecdead2851804b2
cykreet <cykreet@gmail.com>
2025-02-09 12:46:30 +0200

    fix: remove broken threejs minifier

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,8 @@
 import { sveltekit } from "@sveltejs/kit/vite";
 import tailwindcss from "@tailwindcss/vite";
-import { threeMinifier } from "@yushijinhun/three-minifier-rollup";
+// import { threeMinifier } from "@yushijinhun/three-minifier-rollup";
 import { defineConfig } from "vite";
 
 export default defineConfig({
-	plugins: [{ ...threeMinifier(), enforce: "pre" }, tailwindcss(), sveltekit()],
+	plugins: [tailwindcss(), sveltekit()],
 });
