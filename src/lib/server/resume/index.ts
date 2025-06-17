f9526064b950d6e285defec819bd5dc017d0803b
cykreet <cykreet@gmail.com>
2025-06-17 14:35:27 +0200

    fix: use node for hash generation
    bun runtime currently not supported on vercel

--- src/lib/server/resume/index.ts
+++ src/lib/server/resume/index.ts
@@ -1,5 +1,6 @@
 import { RedisMapCache } from "@sylo-digital/kas";
 import { redisConnection } from "../../helpers/get-redis-connection";
+import { randomBytes } from "node:crypto";
 
 export const codeHashMapCache = new RedisMapCache<string>(redisConnection, "resume-codes", {
 	defaultExpiry: "12h",
@@ -8,7 +9,8 @@ export const codeHashMapCache = new RedisMapCache<string>(redisConnection, "resu
 
 export async function requestResumeCode() {
 	const code = crypto.getRandomValues(new Uint16Array(1)).toString();
-	const hash = Bun.hash(Math.random()).toString(16);
+	// const hash = Bun.hash(Math.random()).toString(16);
+	const hash = randomBytes(20).toString("hex");
 	await codeHashMapCache.set(code, hash);
 	return { code, hash };
 }
